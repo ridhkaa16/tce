6f36204ab4fa74960812f7db9f7f9268ef49a5a9
ridhkaa16 <ridhikaranjith@gmail.com>
2024-05-12 21:27:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Custom JavaScript
+$(document).ready(function() {
+    // Add your custom JavaScript here
+    $('#actionButton').click(function() {
+      // Action to perform when the button is clicked
+      alert('ROSE!!!');
+    });
+  });
